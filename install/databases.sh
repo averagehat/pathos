a0c7d04e290960ab0e8579498e2867f86b3a8049
Michael Panciera <mpanciera@head.cluster>
2019-07-05 11:55:17 -0400

    databases.sh now uses make.

--- install/databases.sh
+++ install/databases.sh
@@ -1,6 +1,1 @@
-dir=$HOME/path-pipe
-mkdir -p star-index-dir
-# STAR --runMode genomeGenerate --runThreadN 4 --genomeFastaFiles $dir/hg38.fa $dir/panTro4.fa --genomeDir star-index-dir 
-STAR --runMode genomeGenerate --runThreadN 16 --genomeFastaFiles $dir/hg38.fa $dir/panTro4.fa --genomeDir star-index-dir --limitGenomeGenerateRAM 35085737173
-
-gmap_build -D gmap-conf -d nt-gmap -k 16 $dir/nt2.fasta 
+cd databases && make all
